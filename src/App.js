772dfebe5e2d3a4722fc4bc90562746d604682f7
Toan Trinh <solulink@gmail.com>
2021-09-13 17:28:23 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { useForm } from './hooks/useForm';
+function App() {
+  return (
+    <div>
+      <useForm></useForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useForm.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+const useForm = props => {
+const [state, setState] = useState('');
+    return <div></div>;
+};
+
+useForm.propTypes = {};
+
+export { useForm };
